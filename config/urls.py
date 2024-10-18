71b57454bafff5db2930a2decb2c957c0be6982e
Sumerian <shemerov.s@yandex.ru>
2024-10-18 12:21:32 +0300

    rebase templates

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reserv_service/', include('reserv_service.urls', namespace='reserv_service')),
+    path('', include('reserv_service.urls', namespace='reserv_service')),
     path('users/', include('users.urls', namespace='users')),
 ]
--- reserv_service/urls.py
+++ reserv_service/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from reserv_service.apps import ReservServiceConfig
-
+from reserv_service.views import index
 
 app_name = ReservServiceConfig.name
 
 urlpatterns = [
+    path('', index, name='index'),
     # path("", LoginView.as_view(template_name='users/login.html'), name="login"),
     # path("logout/", LogoutView.as_view(), name="logout"),
     # path("registration/", RegisterView.as_view(), name="registration"),
